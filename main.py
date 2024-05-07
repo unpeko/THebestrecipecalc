56523dff996b939519d0625acf136e96e74635c4
peko <132242919+unpeko@users.noreply.github.com>
2024-05-07 22:30:21 +0000

    added main()

--- main.py
+++ main.py
@@ -18,10 +18,26 @@ def metricpick():
   while True:
     measurment = input("Enter measurment (g, ml): ")
     if measurment == 'g':
-      amount = float(input("Enter amount in grams: "))
-      return amount
+        amount = float(input("Enter amount in grams: "))
+        return amount
     elif measurment == 'ml':
-      amount = float(input("Enter amount in milliliters: "))
-      return amount
+        amount = float(input("Enter amount in milliliters: "))
+        return amount
     else:
-      print("Invalid measurment. Please enter 'g' or 'ml'.")
+        print("Invalid measurment. Please enter 'g' or 'ml'.")
+
+def main():
+    welcom()
+    metricpick()
+    recipe_name = input("Enter recipe name: ")
+    serving_size = input("Enter serving size: ")
+    print(f"Recipe: {recipe_name}, serving size: {serving_size}")
+    while True:
+        ingredient_name = input("Enter ingredient name and when you wan to exit type 'exit' ")
+        if ingredient_name == 'exit':
+            break 
+        ingredients = []
+        ingredients.append(ingredient_name)
+
+    print(ingredients)
+main()
